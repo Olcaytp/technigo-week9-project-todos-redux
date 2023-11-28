f6f58cb46faa0806de16d68ab56f11b25bff2aca
Olcaytp <olcaytapan10@gmail.com>
2023-11-28 20:55:18 +0100

    refactored

--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,10 +1,8 @@
-// rootReducer.js
 import { combineReducers } from 'redux';
 import todoReducer from './todoReducer';
 
 const rootReducer = combineReducers({
   todo: todoReducer,
-  // Diğer reducer'ları ekleyebilirsiniz
 });
 
 export default rootReducer;
